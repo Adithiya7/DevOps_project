c78800b039ee10c9998698a0c1136810530c4bc7
Adithiya7 <aditerraform@gmail.com>
2025-03-10 10:46:09 +0530

    updated app

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! Application works";
+    private final String defaultMessage = "Hello, Spring Boot! Application works!";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, This App works!");
+        return ResponseEntity.ok("Status: OK, This Updated App works!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, App is up");
+            return ResponseEntity.ok("Payload verified: OK, App is up & running!");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
