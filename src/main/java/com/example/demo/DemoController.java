efc2f9452aa95b446f2f3007729fd97ff1473d6c
Adithiya7 <aditerraform@gmail.com>
2025-03-10 09:34:30 +0530

    updated version

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! This is new version";
+    private final String defaultMessage = "Hello, Spring Boot! This is updated version";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, New version Installed!");
+        return ResponseEntity.ok("Status: OK, THIS IS THE UPDATED VERSION!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, New Version");
+            return ResponseEntity.ok("Payload verified: OK, Updated Version");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                                 .body("Error: Invalid payload, Please pass correct values to the application");
+                                 .body("Error: Invalid payload, Please provide correct values to the application");
         }
     }
 }
