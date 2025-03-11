377747bb57d453aeb01a318e8a52536c49a98414
Adithiya7 <aditerraform@gmail.com>
2025-03-11 11:18:26 +0530

    latest version 7

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! Version 7 Live in Production";
+    private final String defaultMessage = "Hello, Spring Boot! Launched Verison 7 Successfully!";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, Production Version 7 Launched Successfully!");
+        return ResponseEntity.ok("Status: OK, Production Version 7 Launched and Live now!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, Production Version 7 is live!");
+            return ResponseEntity.ok("Payload verified: OK, Prod Env Version 7 Up & Live!");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
