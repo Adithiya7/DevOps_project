8fa19731f63755fb0957287332123cdf83966603
Adithiya7 <aditerraform@gmail.com>
2025-03-14 09:00:00 +0530

    Updated config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:11-jre-slim
 WORKDIR /app
-COPY demo-spring-boot-0.0.1-SNAPSHOT.jar /app/my-app.jar
+COPY /app/my-app.jar /app/my-app.jar
 CMD ["java", "-jar", "/app/my-app.jar"]
--- deploy.sh
+++ deploy.sh
@@ -9,25 +9,36 @@ JAR_FILE="demo-spring-boot-0.0.1-SNAPSHOT.jar"
 cd /root/Spring-App/
 
 download_latest_jar() {
-  aws s3 cp s3://$BUCKET_NAME/$JAR_FILE $JAR_PATH
+
+  echo "Removing old JAR..."
+  rm -f $JAR_PATH/my-app.jar $JAR_PATH/$JAR_FILE
+
+  echo "Downloading latest JAR from S3..."
+  aws s3 cp --no-progress --force s3://$BUCKET_NAME/$JAR_FILE $JAR_PATH
   ##mv $JAR_FILE app/my-app.jar
-  mv /root/Spring-App/$JAR_FILE /app/my-app.jar
+  ##mv /root/Spring-App/$JAR_FILE /app/my-app.jar
+  mv $JAR_PATH/$JAR_FILE $JAR_PATH/my-app.jar
+  rm -f app/my-app.jar
+  cp $JAR_PATH/my-app.jar app/my-app.jar
 }
 
 deploy_app() {
 
   # Stop the existing container
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-    echo "Stopping existing container..."
+    echo "Stopping & Removing existing container..."
   docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
+  docker rm -f $CONTAINER_NAME
 
 else
         echo "No existing container found"
 fi
 
+  echo "Removing old untagged images..."
+  docker rmi -f $(docker images -f "dangling=true" -q) 2>/dev/null
+
   # Build & Run a new container with the updated JAR file
-  docker build -t $IMAGE_NAME .
+  docker build --no-cache -t $IMAGE_NAME:latest .
   docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
 }
 
@@ -39,6 +50,6 @@ while true; do
   # Deploy the application
   deploy_app
 
-  # Sleep for a specified interval (e.g., 10 minutes)
-  sleep 300
+  # Sleep for 10mins
+  sleep 600
 done
