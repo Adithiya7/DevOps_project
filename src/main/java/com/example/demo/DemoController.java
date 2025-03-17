a0c0ec69d8fb4a633114d1d3619a9dbc209e8fa9
Adithiya7 <aditerraform@gmail.com>
2025-03-17 18:10:48 +0530

    Version 1.1 Launched

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! Version 11 has passed checks!";
+    private final String defaultMessage = "Hello, Spring Boot! Version 1.1 Launched";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, Latest version 11 Checksum Verified!");
+        return ResponseEntity.ok("Status: OK, Version 1.1 Verified Successfully!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, Version 11 Validated!!!");
+            return ResponseEntity.ok("Payload verified: OK, Version 1.1 Validated");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
