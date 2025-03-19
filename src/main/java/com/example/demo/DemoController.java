187865946befb8e266240c346c56439a998f749f
Adithiya7 <aditerraform@gmail.com>
2025-03-19 19:36:13 +0530

    version 7.1 deployed

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! Latest Version 7 Updated";
+    private final String defaultMessage = "Hello, Spring Boot! Latest Version 7.1 Updated";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, Latest Version 7 launched!");
+        return ResponseEntity.ok("Status: OK, Latest Version 7.1 launched!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, Latest Version 7 Validated!");
+            return ResponseEntity.ok("Payload verified: OK, Latest Version 7.1 Validated!");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
