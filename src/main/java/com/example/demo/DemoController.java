93e7c703f1ef044fee1de84ace06a28f8d6ed782
Adithiya7 <aditerraform@gmail.com>
2025-03-19 12:17:56 +0530

    Jarvis Launched

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class DemoController {
 
     // Define the expected payload message
-    private final String defaultMessage = "Hello, Spring Boot! LATEST Version No.7 Launched";
+    private final String defaultMessage = "Hello, Spring Boot! This is Jarvis";
 
     // GET /status returns a 200 OK response with a simple message
     @GetMapping
     public ResponseEntity<String> getStatus() {
-        return ResponseEntity.ok("Status: OK, LATEST Version No.7 UP Successfully!");
+        return ResponseEntity.ok("Status: OK, Jarvis has been Launched!");
     }
 
     // POST /status validates the payload
     @PostMapping
     public ResponseEntity<String> verifyPayload(@RequestBody Payload payload) {
         if (defaultMessage.equals(payload.getMessage())) {
-            return ResponseEntity.ok("Payload verified: OK, Version 7.7 Validated");
+            return ResponseEntity.ok("Payload verified: OK, Hello from Jarvis!");
         } else {
             // When payload message is not as expected, return an error status code (400 Bad Request)
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
